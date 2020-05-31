87b1c28d5374d0431225620b84d1cac753bc5a67
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2020-05-31 18:15:01 +0300

    Updates

---
+++ precache-manifest.25e4f4ef712a73ef1b5efb48d55207b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b72bae72870cdb104fc1e6eb30a4ba3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4671d8f3fa723f489f7b",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3ee66f3e97744d97d0c7",
+    "url": "/portfolio/static/css/main.d7731be9.chunk.css"
+  },
+  {
+    "revision": "4671d8f3fa723f489f7b",
+    "url": "/portfolio/static/js/2.5f719936.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.5f719936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee66f3e97744d97d0c7",
+    "url": "/portfolio/static/js/main.df648b3e.chunk.js"
+  },
+  {
+    "revision": "27e15bd3e25e8b99391d",
+    "url": "/portfolio/static/js/runtime-main.0df6e538.js"
+  }
+]);
